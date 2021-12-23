6642f7ba1d6c142f4b348795418ab47ddffae290
devjobe <git@devjobe.com>
2021-12-24 00:04:39 +0100

    Add 2015 day 08 solution

---
+++ cmd/day08/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"aoc-2015/internal/puzzle"
+	"strings"
+)
+
+func main() {
+	input := puzzle.ReadLines(2015, 8)
+
+	var decoded int
+	for _, line := range input {
+
+		decoded += 2
+
+		s := line[1 : len(line)-1]
+		for index := strings.Index(s, "\\"); index >= 0; index = strings.Index(s, "\\") {
+			if s[index+1] == 'x' {
+				index += 4
+				decoded += 3
+			} else {
+				index += 2
+				decoded += 1
+			}
+
+			if index >= len(s) {
+				break
+			}
+			s = s[index:]
+		}
+
+	}
+
+	println("Day 07.1:", decoded)
+
+	var encoded int
+	for _, line := range input {
+		encoded += 2 + strings.Count(line, "\\") + strings.Count(line, "\"")
+	}
+	println("Day 07.2:", encoded)
+}
