69cee7edd003b273c40dca1905b381b6d0212056
devjobe <git@devjobe.com>
2021-12-26 04:08:12 +0100

    Add internal.util.Max

--- internal/util/math.go
+++ internal/util/math.go
@@ -1,5 +1,15 @@
 package util
 
+func Max(array ...int64) int64 {
+	max := array[0]
+	for _, n := range array[1:] {
+		if n > max {
+			max = n
+		}
+	}
+	return max
+}
+
 func Min(array ...int64) int64 {
 	min := array[0]
 	for _, n := range array[1:] {
