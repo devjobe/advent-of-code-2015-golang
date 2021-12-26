841c25ccaefff39912742e8925e130a70cfdeef3
devjobe <git@devjobe.com>
2021-12-26 20:39:08 +0100

    Add internal.util.MinInt/MaxInt

--- internal/util/math.go
+++ internal/util/math.go
@@ -1,5 +1,25 @@
 package util
 
+func MaxInt(array ...int) int {
+	max := array[0]
+	for _, n := range array[1:] {
+		if n > max {
+			max = n
+		}
+	}
+	return max
+}
+
+func MinInt(array ...int) int {
+	min := array[0]
+	for _, n := range array[1:] {
+		if n < min {
+			min = n
+		}
+	}
+	return min
+}
+
 func Max(array ...int64) int64 {
 	max := array[0]
 	for _, n := range array[1:] {
