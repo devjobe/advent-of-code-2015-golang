0c1acddbdee77e8dd71d4bfd204b1075a251a247
devjobe <git@devjobe.com>
2021-12-27 15:42:28 +0100

    Add internal.util.CutAfter

--- internal/util/string.go
+++ internal/util/string.go
@@ -6,6 +6,16 @@ func Cut(s, sep string) (string, string, bool) {
 	if index := strings.Index(s, sep); index >= 0 {
 		return s[:index], s[index+len(sep):], true
 	}
+	return s, "", false
+}
 
+func CutAfter(s, sep string, n int) (string, string, bool) {
+	if n >= len(s) {
+		return s, "", false
+	}
+	if index := strings.Index(s[n:], sep); index >= 0 {
+		index += n
+		return s[:index], s[index+len(sep):], true
+	}
 	return s, "", false
 }
